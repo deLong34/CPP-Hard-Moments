71e89bb394d27b68afe1d8f762c04d0e4b5c5626
delong34 <delong@bk.ru>
2022-12-05 17:52:35 +0300

    сдулал 8 урок

--- lesson-1/lesson-1/lesson-1.cpp
+++ lesson-1/lesson-1/lesson-1.cpp
@@ -2,6 +2,34 @@
 #include <Windows.h>
 #include <utility>
 #include "lesson-1.h"
+#include "gtest/gtest-test.h"
+#include "src/gtest.h"
+#include "myTest.h"
+
+template <typename T, typename U>
+void AssertEq(const T& t, const U& u)
+{
+    if (t != u)
+    {
+        ostringstream os;
+        os << "Assertion failed: " << t << "!=" << u;
+        throw runtime_error(os.str());
+    }
+}
+
+template <typename TestFunc>
+void RunTest(TestFunc func, const string& test_name)
+{
+    try
+    {
+        func();
+        cerr << test_name << " OK" << endl;
+    }
+    catch (runtime_error& e) 
+    {
+        cerr << test_name << " FAIL: " << e.what() << endl;
+    }
+}
 
 int main()
 {
@@ -10,13 +38,17 @@ int main()
     cout << book;
     cout << "------SortByPhone-------" << endl;
     book.SotrByPhone();
+    assert(SBPisSorted());
+
     cout << book;
     cout << "------SortByName--------" << endl;
     book.SortByName();
+    assert(SBNisSorted());
+
     cout << book;
     cout << "-----GetPhoneNumber-----" << endl;
     // лямбда функция, которая принимает фамилию и выводит номер телефона человека, либо строку с ошибкой
-        auto print_phone_number = [&book](const string& surname) {
+    auto print_phone_number = [&book](const string& surname) {
         cout << surname << "\t";
         auto answer = book.GetPhoneNumber(surname);
         if (get<0>(answer).empty())
@@ -27,7 +59,9 @@ int main()
     };
     // вызовы лямбды
     print_phone_number("Ivanov");
+    RunTest(print_phone_number("Ivanov"), "print_phone_number()");
     print_phone_number("Petrov");
+    RunTest(print_phone_number("Petrov"), "print_phone_number()");
     cout << "----ChangePhoneNumber----" << endl;
     book.ChangePhoneNumber(Person{ "Kotov", "Vasilii", "Eliseevich" },
         PhoneNumber{ 7, 123, "15344458", nullopt });
@@ -35,4 +69,4 @@ int main()
         PhoneNumber{ 16, 465, "9155448", 13 });
     cout << book;
     return 0;
-}
+};
